bd88e37c916d8238eae1909a174075684020f650
Khoa Bui <buihdk@Khoas-MacBook-Pro.local>
2023-06-19 03:20:54 -0400

    add Bearer Authentication token for when calling Sheety endpoints

--- data_manager.py
+++ data_manager.py
@@ -4,24 +4,35 @@ import dotenv
 
 dotenv.load_dotenv()
 SHEETY_PRICES_ENDPOINT = os.getenv("SHEETY_PRICES_ENDPOINT")
+SHEETY_BEARER_TOKEN = os.getenv("SHEETY_BEARER_TOKEN")
 
 
 class DataManager:
     def __init__(self):
+        self.headers = {"Authorization": f"Bearer {SHEETY_BEARER_TOKEN}"}
         self.destination_data = {}
 
     def get_destination_data(self):
-        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT, headers=self.headers)
         data = response.json()
+
         self.destination_data = data["prices"]
-        # pprint(data) Print formatted data using pretty print
+        # pprint.pprint(data) print data in clearer format
         return self.destination_data
 
     def update_destination_codes(self):
         for city in self.destination_data:
             new_data = {"price": {"iataCode": city["iataCode"]}}
-            requests.put(url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}", json=new_data)
+            requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                headers=self.headers,
+                json=new_data,
+            )
 
     def update_destination_lowest_price(self, row_id, new_price):
         new_data = {"price": {"lowestPrice": new_price}}
-        requests.put(url=f"{SHEETY_PRICES_ENDPOINT}/{row_id}", json=new_data)
+        requests.put(
+            url=f"{SHEETY_PRICES_ENDPOINT}/{row_id}",
+            headers=self.headers,
+            json=new_data,
+        )
